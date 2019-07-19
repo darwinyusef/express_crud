b84aea7ee24af561f139e8460d2a0d4863f227a2
Darwin Yusef Gonzalez Triana <darwin.gonzalez@unibague.edu.co>
2019-07-18 21:48:24 -0500

    install all dependeces chatbot

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+
 const app = express();
 
+app.set('port', 5000);
+app.use(bodyParser.json());
 
-app.get('/', function(req, res, next){
-    res.send('Helloss world');
-});
+app.get('/', function(req, response){
+    response.send('Holas Mundo!');
+})
 
-const server  = app.listen(8000, function(){
-    console.log(`Listening http://localhost:${server.address().port}`);
-});
+app.get('/webhook', function(req, response){
+    if(req.query['hub.verify_token'] === 'cristianismo_token'){
+        response.send(req.query['hub.challenge']);
+    } else {
+        response.send('En Cristianismo Digital no tienes permisos.')
+    }
+})
+
+app.listen(app.get('port'), function(){
+    console.log('Nuestro servidor esta funcionando en el puerto', app.get('port'));
+})
